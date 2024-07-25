b41dc7423dc84015679e43fa89d18649e13666ad
moomoo <anwns1211@gmail.com>
2024-07-25 22:11:54 +0900

    📁 배너 이미지 추가

---
+++ src/components/TodoList.jsx
@@ -0,0 +1,30 @@
+const TodoList = ({ todos, removeTodo, toggleTodo, getTodoDetail }) => {
+  return (
+    <div>
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>
+            <p>id: {todo.id}</p>
+            <p onClick={() => getTodoDetail(todo.id)}>title: {todo.title}</p>
+            <p>userId: {todo.userId}</p>
+            <p>completed: {todo.completed ? "true" : "false"}</p>
+
+            <button
+              onClick={() =>
+                toggleTodo({
+                  ...todo,
+                  completed: !todo.completed,
+                })
+              }
+            >
+              완료
+            </button>
+            <button onClick={() => removeTodo(todo.id)}>삭제</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default TodoList;
